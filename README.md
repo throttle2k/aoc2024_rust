20fa5d307a8ee64ca4e8f6a341dd16b865888745
throttle2k <lomax79@gmail.com>
2024-12-04 16:55:46 +0100

    Day 4: Ceres Search - scaffolding

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ My solutions for Advent of Code 2024 in Rust
 - [x] Day 1: Historian Hysteria
 - [x] Day 2: Red-Nosed Reports
 - [x] Day 3: Mull It Over
+- [ ] Day 4: Ceres Search
---
+++ days/day04/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod day04_tests {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        todo!();
+    }
+}
