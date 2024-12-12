1e67152c2ed77f885705b2c54fba2c7bb147117c
throttle2k <lomax79@gmail.com>
2024-12-13 00:43:22 +0100

    Day 11: Plutonian Pebbles - part 2

--- days/day11/src/main.rs
+++ days/day11/src/main.rs
@@ -1,10 +1,10 @@
-use std::fmt::Display;
+use std::collections::HashMap;
 
 use common::read_input;
 
 #[derive(Debug)]
 struct Stones {
-    pebbles: Vec<u64>,
+    pebbles: HashMap<u64, usize>,
 }
 
 impl From<&str> for Stones {
@@ -12,59 +12,74 @@ impl From<&str> for Stones {
         let pebbles = value
             .trim()
             .split_whitespace()
-            .map(|n| n.parse().unwrap())
-            .collect();
+            .fold(HashMap::new(), |mut pebbles, n| {
+                pebbles
+                    .entry(n.parse().unwrap())
+                    .and_modify(|v| *v += 1)
+                    .or_insert(1);
+                pebbles
+            });
         Self { pebbles }
-    }
-}
-
-impl Display for Stones {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{}",
-            self.pebbles
-                .iter()
-                .map(|n| format!("{n}"))
-                .collect::<Vec<String>>()
-                .join(" ")
-        )
     }
 }
 
 impl Stones {
-    fn step(self) -> Self {
-        let pebbles = self
-            .pebbles
-            .iter()
-            .flat_map(|p| match p {
-                0 => vec![1],
-                n if format!("{n}").len() % 2 == 0 => {
-                    let as_string = format!("{n}");
-                    let (first_half, second_half) = as_string.split_at(as_string.len() / 2);
-                    let first_half = first_half.parse().unwrap();
-                    let second_half = second_half.parse().unwrap();
-                    vec![first_half, second_half]
-                }
-                n => vec![n * 2024],
-            })
-            .collect();
-        Self { pebbles }
+    fn blink(self, n: usize) -> Stones {
+        if n == 0 {
+            return self;
+        }
+        let mut new_pebbles = HashMap::<u64, usize>::new();
+        self.pebbles.iter().for_each(|(k, num)| match k {
+            0 => {
+                new_pebbles
+                    .entry(1)
+                    .and_modify(|v| *v += num)
+                    .or_insert(*num);
+            }
+            n if format!("{n}").len() % 2 == 0 => {
+                let as_string = format!("{n}");
+                let (first_half, second_half) = as_string.split_at(as_string.len() / 2);
+                let first_half = first_half.parse().unwrap();
+                let second_half = second_half.parse().unwrap();
+                new_pebbles
+                    .entry(first_half)
+                    .and_modify(|v| *v += num)
+                    .or_insert(*num);
+                new_pebbles
+                    .entry(second_half)
+                    .and_modify(|v| *v += num)
+                    .or_insert(*num);
+            }
+            n => {
+                new_pebbles
+                    .entry(n * 2024)
+                    .and_modify(|v| *v += num)
+                    .or_insert(*num);
+            }
+        });
+        Self {
+            pebbles: new_pebbles,
+        }
+        .blink(n - 1)
     }
 
-    fn step_for(self, n: usize) -> Self {
-        if n == 0 {
-            self
-        } else {
-            self.step().step_for(n - 1)
-        }
+    fn blink_for(self, n: usize) -> Self {
+        self.blink(n)
     }
 }
 
 fn main() {
     let input = read_input("day11.txt");
     let stones = Stones::from(input.as_str());
-    println!("Part 1 = {}", stones.step_for(25).pebbles.len());
+    println!(
+        "Part 1 = {}",
+        stones.blink_for(25).pebbles.values().sum::<usize>()
+    );
+    let stones = Stones::from(input.as_str());
+    println!(
+        "Part 2 = {}",
+        stones.blink_for(75).pebbles.values().sum::<usize>()
+    );
 }
 
 #[cfg(test)]
@@ -73,23 +88,6 @@ mod day11_tests {
 
     use super::*;
 
-    #[test]
-    fn test_step() {
-        let input = "0 1 10 99 999";
-        let stones = Stones::from(input);
-        assert_eq!(stones.step().to_string(), "1 2024 1 0 9 9 2021976");
-    }
-
-    #[test]
-    fn test_step_for() {
-        let input = "125 17";
-        let stones = Stones::from(input);
-        assert_eq!(
-            stones.step_for(6).to_string(),
-            "2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2"
-        );
-    }
-
     #[parameterized(
         steps = { 1, 2, 3, 4, 5, 6, 25 },
         expected = { 3, 4, 5, 9, 13, 22, 55312 }
@@ -97,6 +95,7 @@ mod day11_tests {
     fn part1(steps: usize, expected: usize) {
         let input = "125 17";
         let stones = Stones::from(input);
-        assert_eq!(stones.step_for(steps).pebbles.len(), expected);
+        let stones = stones.blink_for(steps);
+        assert_eq!(stones.pebbles.values().sum::<usize>(), expected);
     }
 }
