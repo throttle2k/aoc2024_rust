80c3956e7b13c1569a6a1ad1a0554cdddbc905ef
throttle2k <lomax79@gmail.com>
2024-12-14 00:31:52 +0100

    Day 12: Garden Groups - part 2

--- days/day12/src/main.rs
+++ days/day12/src/main.rs
@@ -9,32 +9,15 @@ struct Region {
 }
 
 impl Region {
-    fn new(plant: char, mut plots: Vec<(usize, usize)>) -> Self {
+    fn new(plant: char, mut plots: Vec<(usize, usize)>, as_sides: bool) -> Self {
         plots.sort();
-        let (v_fences, h_fences) = plots.iter().fold(
-            (0, 0),
-            |(mut v_fences, mut h_fences), (plot_row, plot_col)| {
-                if *plot_row == 0 {
-                    h_fences += 1;
-                } else if !plots.contains(&(plot_row - 1, *plot_col)) {
-                    h_fences += 1;
-                }
-                if *plot_col == 0 {
-                    v_fences += 1;
-                } else if !plots.contains(&(*plot_row, plot_col - 1)) {
-                    v_fences += 1;
-                }
-                if !plots.contains(&(plot_row + 1, *plot_col)) {
-                    h_fences += 1;
-                }
-                if !plots.contains(&(*plot_row, plot_col + 1)) {
-                    v_fences += 1;
-                }
-                (v_fences, h_fences)
-            },
-        );
         let area = plots.len();
-        let perimeter = v_fences + h_fences;
+        let (v_fences, h_fences) = fences(&plots);
+        let perimeter = if !as_sides {
+            v_fences.len() + h_fences.len()
+        } else {
+            count_sides(v_fences, h_fences)
+        };
         Self {
             plant,
             plots,
@@ -43,7 +26,7 @@ impl Region {
         }
     }
 
-    fn from(garden: &[&[char]], start: (usize, usize)) -> Self {
+    fn from(garden: &[&[char]], start: (usize, usize), as_sides: bool) -> Self {
         let plant = garden[start.0][start.1];
         let mut queue = vec![start];
         let mut visited = vec![start];
@@ -56,12 +39,139 @@ impl Region {
                 }
             });
         }
-        Self::new(plant, visited)
+        Self::new(plant, visited, as_sides)
     }
 
     fn fence_price(&self) -> usize {
         self.area * self.perimeter
     }
+}
+
+fn fences(
+    plots: &Vec<(usize, usize)>,
+) -> (
+    Vec<((isize, isize), (isize, isize))>,
+    Vec<((isize, isize), (isize, isize))>,
+) {
+    plots.iter().fold(
+        (vec![], vec![]),
+        |(mut v_fences, mut h_fences), (plot_row, plot_col)| {
+            let plot_row = *plot_row as isize;
+            let plot_col = *plot_col as isize;
+            if plot_row == 0 {
+                h_fences.push(((-1, plot_col), (0, plot_col)));
+            } else if !plots.contains(&(plot_row as usize - 1, plot_col as usize)) {
+                h_fences.push(((plot_row - 1, plot_col), (plot_row, plot_col)));
+            }
+            if plot_col == 0 {
+                v_fences.push(((plot_row, -1), (plot_row, 0)));
+            } else if !plots.contains(&(plot_row as usize, plot_col as usize - 1)) {
+                v_fences.push(((plot_row, plot_col - 1), (plot_row, plot_col)));
+            }
+            if !plots.contains(&(plot_row as usize + 1, plot_col as usize)) {
+                h_fences.push(((plot_row + 1, plot_col), (plot_row, plot_col)));
+            }
+            if !plots.contains(&(plot_row as usize, plot_col as usize + 1)) {
+                v_fences.push(((plot_row, plot_col + 1), (plot_row, plot_col)));
+            }
+            (v_fences, h_fences)
+        },
+    )
+}
+
+fn fences_v_sides(fences: &mut Vec<((isize, isize), (isize, isize))>) -> usize {
+    let mut sides = 0;
+
+    while !fences.is_empty() {
+        let (current_outer, current_inner) = fences.remove(0);
+
+        let mut same_axis = fences
+            .iter()
+            .filter(|(other_outer, other_inner)| {
+                other_outer.1 == current_outer.1 && other_inner.1 == current_inner.1
+            })
+            .collect::<Vec<_>>();
+        same_axis.sort_by(|(a, _), (b, _)| a.0.cmp(&b.0));
+
+        let min_index = if current_outer.0 > 0 {
+            (0..current_outer.0)
+                .rev()
+                .take_while(|index| {
+                    same_axis.contains(&&((*index, current_outer.1), (*index, current_inner.1)))
+                })
+                .last()
+                .unwrap_or(current_outer.0)
+        } else {
+            current_outer.0
+        };
+
+        let max_index = (current_outer.0 + 1..)
+            .take_while(|index| {
+                same_axis.contains(&&((*index, current_outer.1), (*index, current_inner.1)))
+            })
+            .last()
+            .unwrap_or(current_outer.0);
+
+        (min_index..=max_index)
+            .map(|index| ((index, current_outer.1), (index, current_inner.1)))
+            .for_each(|same_side| fences.retain(|side| *side != same_side));
+
+        sides += 1;
+    }
+    sides
+}
+
+fn fences_h_sides(fences: &mut Vec<((isize, isize), (isize, isize))>) -> usize {
+    let mut sides = 0;
+
+    while !fences.is_empty() {
+        let (current_outer, current_inner) = fences.remove(0);
+
+        let mut same_axis = fences
+            .iter()
+            .filter(|(other_outer, other_inner)| {
+                other_outer.0 == current_outer.0 && other_inner.0 == current_inner.0
+            })
+            .collect::<Vec<_>>();
+        same_axis.sort_by(|(a, _), (b, _)| a.1.cmp(&b.1));
+
+        let min_index = if current_outer.1 > 0 {
+            (0..current_outer.1)
+                .rev()
+                .take_while(|index| {
+                    same_axis.contains(&&((current_outer.0, *index), (current_inner.0, *index)))
+                })
+                .last()
+                .unwrap_or(current_outer.1)
+        } else {
+            current_outer.1
+        };
+
+        let max_index = (current_outer.1 + 1..)
+            .take_while(|index| {
+                same_axis.contains(&&((current_outer.0, *index), (current_inner.0, *index)))
+            })
+            .last()
+            .unwrap_or(current_outer.1);
+
+        (min_index..=max_index)
+            .map(|index| ((current_outer.0, index), (current_inner.0, index)))
+            .for_each(|same_side| fences.retain(|side| *side != same_side));
+
+        sides += 1;
+    }
+    sides
+}
+
+fn count_sides(
+    mut v_fences: Vec<((isize, isize), (isize, isize))>,
+    mut h_fences: Vec<((isize, isize), (isize, isize))>,
+) -> usize {
+    let vertical_sides = fences_v_sides(&mut v_fences);
+
+    let horizontal_sides = fences_h_sides(&mut h_fences);
+
+    vertical_sides + horizontal_sides
 }
 
 fn neighbors(garden: &[&[char]], (plot_row, plot_col): (usize, usize)) -> Vec<(usize, usize)> {
@@ -100,8 +210,8 @@ struct Garden {
     regions: Vec<Region>,
 }
 
-impl From<&str> for Garden {
-    fn from(value: &str) -> Self {
+impl Garden {
+    fn from(value: &str, as_sides: bool) -> Self {
         let plots = value
             .trim()
             .lines()
@@ -114,7 +224,7 @@ impl From<&str> for Garden {
             .flat_map(|(row, r)| {
                 r.iter()
                     .enumerate()
-                    .map(|(col, _)| Region::from(&plots, (row, col)))
+                    .map(|(col, _)| Region::from(&plots, (row, col), as_sides))
                     .collect::<Vec<Region>>()
             })
             .collect::<Vec<Region>>();
@@ -122,9 +232,7 @@ impl From<&str> for Garden {
         regions.dedup();
         Self { regions }
     }
-}
 
-impl Garden {
     fn fence_price(&self) -> usize {
         self.regions.iter().map(|r| r.fence_price()).sum()
     }
@@ -132,8 +240,10 @@ impl Garden {
 
 fn main() {
     let input = read_input("day12.txt");
-    let garden = Garden::from(input.as_str());
+    let garden = Garden::from(input.as_str(), false);
     println!("Part 1 = {}", garden.fence_price());
+    let garden = Garden::from(input.as_str(), true);
+    println!("Part 2 = {}", garden.fence_price());
 }
 
 #[cfg(test)]
@@ -146,7 +256,7 @@ mod day12_tests {
     fn test_parse_input_1() {
         let input = r#"AA
 AA"#;
-        let garden = Garden::from(input);
+        let garden = Garden::from(input, false);
         assert_eq!(garden.regions.len(), 1);
         let region = garden.regions.get(0).unwrap();
         assert_eq!(region.perimeter, 8);
@@ -157,7 +267,7 @@ AA"#;
     fn test_parse_input_2() {
         let input = r#"AAAA
 BBBB"#;
-        let garden = Garden::from(input);
+        let garden = Garden::from(input, false);
         assert_eq!(garden.regions.len(), 2);
         let region1 = garden.regions.get(0).unwrap();
         assert_eq!(region1.perimeter, 10);
@@ -173,7 +283,7 @@ BBBB"#;
 BBCD
 BBCC
 EEEC"#;
-        let garden = Garden::from(input);
+        let garden = Garden::from(input, false);
         assert_eq!(garden.regions.len(), 5);
     }
 
@@ -182,7 +292,7 @@ EEEC"#;
         expected = { 40, 32, 40, 4, 24 }
     )]
     fn test_region_fence_price(input: Vec<(usize, usize)>, expected: usize) {
-        let region = Region::new('A', input);
+        let region = Region::new('A', input, false);
         assert_eq!(region.fence_price(), expected);
     }
 
@@ -192,7 +302,7 @@ EEEC"#;
 BBCD
 BBCC
 EEEC"#;
-        let garden = Garden::from(input);
+        let garden = Garden::from(input, false);
         assert_eq!(garden.fence_price(), 140);
     }
 
@@ -203,7 +313,7 @@ OXOXO
 OOOOO
 OXOXO
 OOOOO"#;
-        let garden = Garden::from(input);
+        let garden = Garden::from(input, false);
         assert_eq!(garden.fence_price(), 772);
     }
 
@@ -219,7 +329,114 @@ VVIIICJJEE
 MIIIIIJJEE
 MIIISIJEEE
 MMMISSJEEE"#;
-        let garden = Garden::from(input);
+        let garden = Garden::from(input, false);
         assert_eq!(garden.fence_price(), 1930);
+    }
+
+    #[test]
+    fn test_fences() {
+        let input = vec![(0, 0), (0, 1)];
+        let (v_fences, h_fences) = fences(&input);
+        assert_eq!(v_fences.len(), 2);
+        assert!(v_fences.contains(&((0, -1), (0, 0))));
+        assert!(v_fences.contains(&((0, 2), (0, 1))));
+        assert_eq!(h_fences.len(), 4);
+        assert!(h_fences.contains(&((-1, 0), (0, 0))));
+        assert!(h_fences.contains(&((1, 0), (0, 0))));
+        assert!(h_fences.contains(&((-1, 1), (0, 1))));
+        assert!(h_fences.contains(&((1, 1), (0, 1))));
+    }
+
+    #[parameterized(
+        input = { vec![(0,0)], vec![(0,0), (0,1)], vec![(0,0), (0,1), (1,0), (1,1)], vec![(0,0), (1,0), (1,1), (2,1)] },
+        expected_v = {2, 2, 2, 4},
+    )]
+    fn test_fences_v_sides(input: Vec<(usize, usize)>, expected_v: usize) {
+        let (mut v_fences, _) = fences(&input);
+        let v_sides = fences_v_sides(&mut v_fences);
+        assert_eq!(v_sides, expected_v);
+    }
+
+    #[parameterized(
+        input = { vec![(0,0)], vec![(0,0), (0,1)], vec![(0,0), (0,1), (1,0), (1,1)], vec![(0,0), (1,0), (1,1), (2,1)] },
+        expected_h = {2, 2, 2, 4},
+    )]
+    fn test_fences_h_sides(input: Vec<(usize, usize)>, expected_h: usize) {
+        let (_, mut h_fences) = fences(&input);
+        let h_sides = fences_h_sides(&mut h_fences);
+        assert_eq!(h_sides, expected_h);
+    }
+
+    #[parameterized(
+        input = { vec![(0,0)], vec![(0,0), (0,1)], vec![(0,0), (0,1), (1,0), (1,1)], vec![(0,0), (1,0), (1,1), (2,1)] },
+        expected_v = {2, 2, 2, 4},
+        expected_h = {2, 2, 2, 4},
+    )]
+    fn test_sides(input: Vec<(usize, usize)>, expected_v: usize, expected_h: usize) {
+        let (mut v_fences, mut h_fences) = fences(&input);
+        let v_sides = fences_v_sides(&mut v_fences);
+        let h_sides = fences_h_sides(&mut h_fences);
+        assert_eq!(v_sides, expected_v);
+        assert_eq!(h_sides, expected_h);
+    }
+
+    #[test]
+    fn test_fence_price_as_sides_1() {
+        let input = r#"AAAA
+BBCD
+BBCC
+EEEC"#;
+        let garden = Garden::from(input, true);
+        assert_eq!(garden.fence_price(), 80);
+    }
+
+    #[test]
+    fn test_fence_price_as_sides_2() {
+        let input = r#"EEEEE
+EXXXX
+EEEEE
+EXXXX
+EEEEE"#;
+        let garden = Garden::from(input, true);
+        assert_eq!(garden.fence_price(), 236);
+    }
+
+    #[test]
+    fn test_fence_price_as_sides_3() {
+        let input = r#"OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO"#;
+        let garden = Garden::from(input, true);
+        assert_eq!(garden.fence_price(), 436);
+    }
+
+    #[test]
+    fn test_fence_price_as_sides_4() {
+        let input = r#"AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA"#;
+        let garden = Garden::from(input, true);
+        assert_eq!(garden.fence_price(), 368);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = r#"RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"#;
+        let garden = Garden::from(input, true);
+        assert_eq!(garden.fence_price(), 1206);
     }
 }
