40c00697061bc37c197e43a634508cf911a56aaf
throttle2k <lomax79@gmail.com>
2024-12-25 17:45:57 +0100

    Day 22: Monkey Market - part 2

--- days/day22/src/main.rs
+++ days/day22/src/main.rs
@@ -1,4 +1,7 @@
+use std::collections::{HashMap, HashSet};
+
 use common::read_input;
+use itertools::Itertools;
 
 fn mix(num: usize, val: usize) -> usize {
     val ^ num
@@ -36,7 +39,6 @@ fn process(secret_num: usize) -> usize {
         .unwrap()
 }
 
-#[allow(dead_code)]
 fn peek_next_numbers(secret_num: usize, n: usize) -> Vec<usize> {
     let (result, _) = (0..n).fold((vec![], secret_num), |(mut result, secret_num), _| {
         let secret_num = process(secret_num);
@@ -46,8 +48,50 @@ fn peek_next_numbers(secret_num: usize, n: usize) -> Vec<usize> {
     result
 }
 
-fn process_next_numbers(secret_num: usize, n: usize) -> usize {
-    (0..n).fold(secret_num, |secret_num, _| process(secret_num))
+fn to_price(secret_num: usize) -> usize {
+    secret_num % 10
+}
+
+fn to_deltas(prices: Vec<usize>) -> Vec<(usize, isize)> {
+    prices
+        .iter()
+        .tuple_windows()
+        .map(|(&prev, &next)| (next, next as isize - prev as isize))
+        .collect()
+}
+
+fn to_bananas_map(deltas: Vec<(usize, isize)>) -> HashMap<Vec<isize>, usize> {
+    deltas
+        .windows(4)
+        .map(|ds| {
+            let deltas = vec![ds[0].1, ds[1].1, ds[2].1, ds[3].1];
+            let price = ds[3].0;
+            (price, deltas)
+        })
+        .fold(HashMap::new(), |mut bananas_map, (price, deltas)| {
+            bananas_map.entry(deltas.to_vec()).or_insert(price);
+            bananas_map
+        })
+}
+
+fn find_best_sequences_for_bananas(maps: Vec<HashMap<Vec<isize>, usize>>) -> usize {
+    let sequences = maps
+        .iter()
+        .fold(HashSet::new(), |mut sequences, bananas_map| {
+            bananas_map.keys().cloned().for_each(|key| {
+                sequences.insert(key);
+            });
+            sequences
+        });
+    sequences
+        .iter()
+        .map(|seq| {
+            maps.iter()
+                .map(|bananas_map| bananas_map.get(seq).unwrap_or(&0))
+                .sum()
+        })
+        .max()
+        .unwrap_or_default()
 }
 
 fn process_multiple(input: &str) -> usize {
@@ -55,13 +99,45 @@ fn process_multiple(input: &str) -> usize {
         .trim()
         .lines()
         .map(|l| l.parse().unwrap())
-        .map(|secret_num| process_next_numbers(secret_num, 2000))
+        .map(|secret_num| *peek_next_numbers(secret_num, 2000).last().unwrap())
         .sum()
+}
+
+fn part2(input: &str) -> usize {
+    let secret_nums_vec = input
+        .trim()
+        .lines()
+        .map(|l| l.parse().unwrap())
+        .map(|secret_num| {
+            let mut v = vec![secret_num];
+            v.extend(peek_next_numbers(secret_num, 1999));
+            v
+        })
+        .collect::<Vec<_>>();
+    let prices = secret_nums_vec
+        .iter()
+        .map(|secret_nums| {
+            secret_nums
+                .iter()
+                .map(|&secret_num| to_price(secret_num))
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let deltas_vec = prices
+        .iter()
+        .map(|secret_nums| to_deltas(secret_nums.to_vec()))
+        .collect::<Vec<_>>();
+    let bananas_maps = deltas_vec
+        .iter()
+        .map(|deltas| to_bananas_map(deltas.to_vec()))
+        .collect::<Vec<_>>();
+    find_best_sequences_for_bananas(bananas_maps)
 }
 
 fn main() {
     let input = read_input("day22.txt");
     println!("Part 1 = {}", process_multiple(input.as_str()));
+    println!("Part 2 = {}", part2(input.as_str()));
 }
 
 #[cfg(test)]
@@ -101,5 +177,100 @@ mod day22_tests {
 100
 2024"#;
         assert_eq!(process_multiple(input), 37327623);
+    }
+
+    #[test]
+    fn test_to_price() {
+        let mut secret_nums = vec![123];
+        secret_nums.extend(peek_next_numbers(123, 9));
+        let prices = secret_nums
+            .iter()
+            .map(|&secret_num| to_price(secret_num))
+            .collect::<Vec<_>>();
+        assert_eq!(prices, vec![3, 0, 6, 5, 4, 4, 6, 4, 4, 2]);
+    }
+
+    #[test]
+    fn test_to_deltas() {
+        let mut secret_nums = vec![123];
+        secret_nums.extend(peek_next_numbers(123, 9));
+        let prices = secret_nums
+            .iter()
+            .map(|&secret_num| to_price(secret_num))
+            .collect::<Vec<_>>();
+        let deltas = to_deltas(prices);
+        assert_eq!(
+            deltas,
+            vec![
+                (0, -3),
+                (6, 6),
+                (5, -1),
+                (4, -1),
+                (4, 0),
+                (6, 2),
+                (4, -2),
+                (4, 0),
+                (2, -2)
+            ]
+        );
+    }
+
+    #[test]
+    fn test_to_bananas_maps() {
+        let input = r#"1
+2
+3
+2024"#;
+        let secret_nums_vec = input
+            .trim()
+            .lines()
+            .map(|l| l.parse().unwrap())
+            .map(|secret_num| {
+                let mut v = vec![secret_num];
+                v.extend(peek_next_numbers(secret_num, 1999));
+                v
+            })
+            .collect::<Vec<_>>();
+        let prices = secret_nums_vec
+            .iter()
+            .map(|secret_nums| {
+                secret_nums
+                    .iter()
+                    .map(|&secret_num| to_price(secret_num))
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
+        let deltas_vec = prices
+            .iter()
+            .map(|secret_nums| to_deltas(secret_nums.to_vec()))
+            .collect::<Vec<_>>();
+        let bananas_maps = deltas_vec
+            .iter()
+            .map(|deltas| to_bananas_map(deltas.to_vec()))
+            .collect::<Vec<_>>();
+        assert_eq!(bananas_maps.len(), 4);
+        let seq: Vec<isize> = vec![-2, 1, -1, 3];
+        assert_eq!(
+            bananas_maps.iter().nth(0).unwrap().get(&seq),
+            Some(7).as_ref()
+        );
+        assert_eq!(
+            bananas_maps.iter().nth(1).unwrap().get(&seq),
+            Some(7).as_ref()
+        );
+        assert_eq!(bananas_maps.iter().nth(2).unwrap().get(&seq), None.as_ref());
+        assert_eq!(
+            bananas_maps.iter().nth(3).unwrap().get(&seq),
+            Some(9).as_ref()
+        );
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = r#"1
+2
+3
+2024"#;
+        assert_eq!(part2(input), 23);
     }
 }
